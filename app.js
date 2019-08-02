8fc7e58936aeb504e7c0c030d0942339edede9ad
MaxPower <mxmcoursey@gmail.com>
2019-08-02 09:18:38 -0400

    updated app.js file with working API query, test.js is duplicate code

--- app.js
+++ app.js
@@ -4,38 +4,53 @@
  * 
  */
 
+// const API_KEY = "api_key=5WD1Tc70yflyZBAXRMHZzg1p6lUF0Nbm" //Face++ Key
+// const API_SECRET = "api_secret=RvXCsEc7vf6xZFr-q1h1KH_F0hJ9vKzm" //Face++ Key
 
-//Face++ API key and secret as required by API
-const API_KEY = "api_key=5WD1Tc70yflyZBAXRMHZzg1p6lUF0Nbm"
-const API_SECRET = "api_secret=RvXCsEc7vf6xZFr-q1h1KH_F0hJ9vKzm"
 
-const queryDrinkURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php";  //NOTE - the "1" is a temp developer key need to request new key if we publish to app store
-const queryFaceURL = "https://api-us.faceplusplus.com/facepp/v3/detect/?"  //TODO not sure why this is not working right now - getting API NOT FOUND 404 error
+var form = new FormData();
+form.append("api_key", "5WD1Tc70yflyZBAXRMHZzg1p6lUF0Nbm");
+form.append("api_secret", "RvXCsEc7vf6xZFr-q1h1KH_F0hJ9vKzm");
+form.append("image_url", "https://images-na.ssl-images-amazon.com/images/I/61Wo915nuTL._SL1000_.jpg");
+form.append("return_attributes", "emotion");
+// form.append("return_attributes", "gender"); //API seems to only take one attribute section as an input
 
-let img_URL = "" //TODO temp variable for file upload URL if needed
-let imgFile = "" //TODO temp file name of uploaded image
-let ingredient = "" //TODO parameter to pass to cocktail DB ajax query
-let idDrink = "" //TODO ID pulled from DB based on the main base alchohol
+var settings = {
+    "url": "https://api-us.faceplusplus.com/facepp/v3/detect",
+    "method": "POST",
+    "processData": false,
+    "contentType": false,
+    "mimeType": "multipart/form-data",
+    "data": form
+}
 
-const queryIngredientURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + ingredient;
-const queryDrinkURL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=16943" + idDrink
+$.ajax(settings).done(function (response) {
+    const face = JSON.parse(response);
+    const tempFace = JSON.parse(response);
+    const emotions = face.faces[0].attributes.emotion;
+    sortedEmotions = [];
 
-//intial call to cocktail DB
-$.ajax({
-    url: queryDrinkURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
+    console.log(emotions);
+
+    for (var type in emotions) {
+        sortedEmotions.push([type, emotions[type]])
+    }
+    sortedEmotions.sort(function (a, b) {
+        return b[1] - a[1];
+    });
+
+    console.log("Primary: " + sortedEmotions[0][0] + ": " + sortedEmotions[0][1]);
+    console.log("Secondary: " + sortedEmotions[1][0] + ": " + sortedEmotions[1][1]);
+
+    //OLD CODE TO GRAB MAX and SECOND TO MAX KEYS AND VALUES
+    // const tempEmotions = tempFace.faces[0].attributes.emotion;  
+    // const primaryKey = Object.keys(emotions).reduce((a, b) => emotions[a] > emotions[b] ? a : b);
+    // const primaryVal = emotions[primaryKey];
+    // delete tempEmotions[primaryKey];
+    // const secondaryKey = Object.keys(tempEmotions).reduce((a, b) => tempEmotions[a] > tempEmotions[b] ? a : b);
+    // const secondaryVal = emotions[secondaryKey];
+    // console.log(emotions);
+    // console.log(primaryKey + ": " + emotions[primaryKey]);
+    // console.log(secondaryKey + ": " + emotions[secondaryKey]);
+
 });
-
-
-//initial call to Face++
-$.ajax({
-    url: queryFaceURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-});
-
-
-
--- test.js
+++ test.js
@@ -1,22 +1,52 @@
 
 
-const API_KEY = "api_key=5WD1Tc70yflyZBAXRMHZzg1p6lUF0Nbm"
-const API_SECRET = "api_secret=RvXCsEc7vf6xZFr-q1h1KH_F0hJ9vKzm"
-const img = "images/max.jpg"
+// const API_KEY = "api_key=5WD1Tc70yflyZBAXRMHZzg1p6lUF0Nbm" //Face++ Key
+// const API_SECRET = "api_secret=RvXCsEc7vf6xZFr-q1h1KH_F0hJ9vKzm" //Face++ Key
 
-$.ajax({
-    url: "https://api-us.faceplusplus.com/facepp/v3/detect/", 
-    method: "POST",
-    headers: {
-        "x-rapidapi-host": "faceplusplus-faceplusplus.p.rapidapi.com",
-        "x-rapidapi-key": "dbbd80497bmsh91d0d2e1a4a10fap1f2952jsn61b1a861f8af",
-        "content-type": "application/x-www-form-urlencoded",    
-        "Access-Control-Allow-Origin" : "*"
-    },
-    data: {
-        "image_url": "https://images-na.ssl-images-amazon.com/images/I/61Wo915nuTL._SL1000_.jpg"
+
+var form = new FormData();
+form.append("api_key", "5WD1Tc70yflyZBAXRMHZzg1p6lUF0Nbm");
+form.append("api_secret", "RvXCsEc7vf6xZFr-q1h1KH_F0hJ9vKzm");
+form.append("image_url", "https://images-na.ssl-images-amazon.com/images/I/61Wo915nuTL._SL1000_.jpg");
+form.append("return_attributes", "emotion");
+// form.append("return_attributes", "gender"); //API seems to only take one attribute section as an input
+
+var settings = {
+    "url": "https://api-us.faceplusplus.com/facepp/v3/detect",
+    "method": "POST",
+    "processData": false,
+    "contentType": false,
+    "mimeType": "multipart/form-data",
+    "data": form
+}
+
+$.ajax(settings).done(function (response) {
+    const face = JSON.parse(response);
+    const tempFace = JSON.parse(response);
+    const emotions = face.faces[0].attributes.emotion;
+    sortedEmotions = [];
+
+    console.log(emotions);
+
+    for (var type in emotions) {
+        sortedEmotions.push([type, emotions[type]])
     }
-}).then(function (response) {
-    console.log(response)
-});
+    sortedEmotions.sort(function (a, b) {
+        return b[1] - a[1];
+    });
 
+    console.log("Primary: " + sortedEmotions[0][0] + ": " + sortedEmotions[0][1]);
+    console.log("Secondary: " + sortedEmotions[1][0] + ": " + sortedEmotions[1][1]);
+
+    //OLD CODE TO GRAB MAX and SECOND TO MAX KEYS AND VALUES
+    // const tempEmotions = tempFace.faces[0].attributes.emotion;  
+    // const primaryKey = Object.keys(emotions).reduce((a, b) => emotions[a] > emotions[b] ? a : b);
+    // const primaryVal = emotions[primaryKey];
+    // delete tempEmotions[primaryKey];
+    // const secondaryKey = Object.keys(tempEmotions).reduce((a, b) => tempEmotions[a] > tempEmotions[b] ? a : b);
+    // const secondaryVal = emotions[secondaryKey];
+    // console.log(emotions);
+    // console.log(primaryKey + ": " + emotions[primaryKey]);
+    // console.log(secondaryKey + ": " + emotions[secondaryKey]);
+
+});
