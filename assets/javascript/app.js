a765ab6fa5a765af5dce5f54b6a289b670de0a65
arsanson <arsanson@mindspring.com>
2019-08-02 12:35:01 -0400

    comments out all console.logs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,8 +13,8 @@ file.onchange = function () {previewFile()};
 
 function submitPicture(picture) {
     //matt coming up with this. will nest once complete? Just console.logging picture name for now
-    console.log("picture submited")
-    console.log(picture) // logs img name only to console
+    //console.log("picture submited")
+    //console.log(picture) // logs img as url to console
     
 }
 
@@ -26,7 +26,7 @@ function previewFile(){
 
     reader.onloadend = function () {
         preview.src = reader.result;
-        console.log(reader.result) //logs img as url to console
+        //console.log(reader.result) //logs img as url to console
         $("#pictureSubmit").on("click", function () { submitPicture(reader.result) })
     }
 
